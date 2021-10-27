a6628a297b96052152993431ed3ad4292a2eecf9
1000099627 <adityasrivastav76@gmail.com>
2021-10-27 13:21:28 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from "./MyComponent/header";
+import Codeo from "./MyComponent/codeo";
+import Footer from "./MyComponent/footer";
+
+
+
+function App() {
+  return (
+    <>
+  <Header/>
+    
+        <Codeo/>
+        <Footer/>
+        
+    
+    </>
+
+  );
+}
+
+export default App;
